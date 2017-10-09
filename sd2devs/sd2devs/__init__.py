0a591033634254c57e820bc28c29adb5c0ba9fd3
Modrow <hemodrow@tmoviles.com.ar>
2017-10-09 08:54:07 -0300

    Estructura base para hacer pruebas

--- sd2devs/sd2devs/__init__.py
+++ sd2devs/sd2devs/__init__.py
@@ -2,5 +2,7 @@
 Modulo principal
 '''
 
-from .sd2devs import read_xml, dag_from_xmile_model, xmile_model_dag_to_devsml, devsml_from_xmile
-from .utils import show_first_elements_diff, elements_equal
+from .sd2devs import dag_from_xmile_model
+from .sd2devs import xmile_model_dag_to_devsml, devsml_from_xmile
+from .xml_utils import read_xml, show_first_elements_diff, elements_equal
+from .devsml2ma import *
---
+++ sd2devs/tests/test_devsml2ma.py
@@ -0,0 +1,18 @@
+"""
+    Prueba de conversion de devsml a ma
+"""
+import sys
+import unittest
+
+sys.path.append('D:\\simu\\sed2017\\tps\\converter\\sd2devs')
+
+
+class TestXmileConverter(unittest.TestCase):
+    '''
+    Pruebas del conversor en si
+    '''
+    def test_convert_devsml_to_ma(self):
+        """
+        Prueba que la conversion de xmile a devsml
+        """
+        pass
