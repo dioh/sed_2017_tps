67d2329638f1c65745de895990ea4e58a019696d
dioh <dj.foguelman@gmail.com>
2017-10-22 16:46:35 -0300

    merge

--- modelos-ds/teacup/runModel.py
+++
@@ -1,5 +0,0 @@
-import pysd
-
-TEACUP_MODEL = pysd.read_vensim('teacup.mdl')
-
-TEACUP_MODEL.run(params={'Room Temperature':70, 'Characteristic Time':100}, initial_condition=(0, {'Teacup Temperature':80}), return_timestamps=range(0,180), return_columns=['Characteristic Time', 'Heat Loss to Room', 'Room Temperature', 'Teacup Temperature']).to_csv('teacup_sd.csv', sep=",", encoding='utf-8')
