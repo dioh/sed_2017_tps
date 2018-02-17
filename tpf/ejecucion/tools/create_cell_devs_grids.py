ce8d7d993b11d12e90e6ea8fe48aa482c12442ba
Modrow <hemodrow@tmoviles.com.ar>
2018-02-17 15:13:23 -0300

    Bug fixing vario. Genera codigo archivo .val en base a configuracion.

--- tpf/ejecucion/tools/create_cell_devs_grids.py
+++ tpf/ejecucion/tools/create_cell_devs_grids.py
@@ -13,6 +13,7 @@ except (ImportError, AttributeError):
 import configparser
 import random
 import functools
+from collections import deque
 
 logger = logging.getLogger(__name__)
 
@@ -20,38 +21,53 @@ logger = logging.getLogger(__name__)
 def get_config(config_file_path):
     with config_file_path.open() as config_file:
         config = configparser.ConfigParser()
-        config.read(config_file)
+        config.read_file(config_file)
+        logger.info('Config readed. Sections %s', config.sections())
         return config
 
 
 def get_dimensions(config):
-    return config['grid']['dimensions'].split(' ')
+    if 'grid' in config:
+        return list(map(lambda d: int(d), config['grid']['dimensions'].split(' ')))
+    else:
+        raise KeyError('grid section not found. Sections: {0}'.format(config.sections()))
+
+def get_group_names(config):
+    sections = config.sections()
+    sections.remove('grid')
+    return sections
 
 
 def get_groups(config, dimensions):
     total_cells = functools.reduce(lambda x, y: x*y, dimensions)
+    group_names = get_group_names(config)
+    logger.debug('Group names: %s', group_names)
     groups = dict()
-    for group in config.sections().remove('grid'):
-        groups[group] = {'min': config.getfloat(group, 'min value'),
-                         'max': config.getfloat(group, 'max value'),
-                         'porcentage': config.getfloat(group, 'porcentage'),
-                         'quantity':
-                         round(config.getfloat(group, 'porcentage') *
-                               total_cells)}
+    for group_name in group_names:
+        groups[group_name] = {'min': config.getfloat(group_name, 'min value'),
+                              'max': config.getfloat(group_name, 'max value'),
+                              'porcentage': config.getfloat(group_name, 'porcentage'),
+                              'quantity':
+                                round(config.getfloat(group_name, 'porcentage') *
+                                      total_cells / 100)}
 
     return groups
 
 
 def generate_group_values(group):
     values = []
-    for _ in range(0, group['quantity']):
+    logger.debug('group: %s', group)
+    for i in range(0, group['quantity']):
+        logger.debug('i: %i', i)
         values.append(random.uniform(group['min'], group['max']))
+    
+    return values
 
 
 def get_shuffled_group_values(groups):
     values = []
-    for group in groups.values:
-        values.append(generate_group_values(group))
+    for group in groups.values():
+        values = values + generate_group_values(group)
 
     random.shuffle(values)
 
@@ -59,19 +75,26 @@ def get_shuffled_group_values(groups):
 
 
 def create_cells(dimensions):
-    cells = ['(']
+    cells = deque(['('] * dimensions[0])
+    logger.debug('dimensions first value: %s', dimensions[0])
+    logger.debug('cells: %s', cells)
     for dimension in dimensions:
-        for i in range(0, dimension):
-            cell = cells.pop()
-            cell = '{0}{1},'.format(cell, i)
+        for i in range(0, len(cells)):
+            cell = cells.popleft()
+            for j in range(0, dimension):
+                new_cell = '{0}{1},'.format(cell, j)
+                logger.debug('dimension: %i; iteration: %i, cell: %s', i, j, new_cell)
+                cells.append(new_cell)
 
-    return list(map(lambda cell: cell[:-1]+')', cells))
+    return list(map(lambda cell: cell[:-1]+')', list(cells)))
 
 
 def create_grid_file(dimensions, groups, file_path):
 
     cells = create_cells(dimensions)
+    logger.debug('len cells: %i', len(cells))
     values = get_shuffled_group_values(groups)
+    logger.debug('len values: %i', len(values))
 
     with open(file_path, 'w') as outfile:
         for cell, value in map(lambda x, y: (x, y), cells, values):
@@ -81,14 +104,16 @@ def create_grid_file(dimensions, groups, file_path):
 def create_groups_str(groups):
     group_strs = []
     for group in groups.keys():
+        logger.debug('group: %s', group)
+        logger.debug('group values: %s', groups[group])
         group_strs.append('{0}_{1}'.format(group,
-                                           groups[groups]['porcentage']))
+                                           groups[group]['porcentage']))
 
     return '_'.join(group_strs)
 
 
 def build_file_path(output_dir, outfiles_prefix, i, dimensions, groups):
-    dimensions_str = '_'.join(dimensions)
+    dimensions_str = '_'.join(list(map(lambda d: d.__str__(), dimensions)))
     groups_str = create_groups_str(groups)
     filename = '{0}_{1}_{2}_{3}.val'.format(outfiles_prefix, i,
                                             dimensions_str, groups_str)
@@ -102,34 +127,44 @@ def create_grids(config_file_path, args):
     groups = get_groups(config, dimensions)
 
     for i in range(args.cases):
-        file_path = build_file_path(args.output_dir, args.outfiles_prefix,
+        file_path = build_file_path(args.outdir, args.outfile_prefix,
                                     i, dimensions, groups)
         create_grid_file(dimensions, groups,
                          file_path)
+
+
+def config_logging():
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+            '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
 
 
 def main():
     """
     Bloque principal
     """
+    config_logging()
     parser = argparse.ArgumentParser(
         description="Crea un archivo .val de valores iniciales de una grilla Cell-DEVS.")
-    parser.add_argument("config_file",
+    parser.add_argument("-c", "--config",
                         help="Archivo de configuracion de los parametros de la grilla")
-    parser.add_argument("outfiles_prefix",
+    parser.add_argument("-p", "--outfile_prefix",
                         help="Prefijo de los archivos de salida")
-    parser.add_argument("cases",
-                        help="Cantidad de grillas a generar")
-    parser.add_argument("output_dir",
+    parser.add_argument("-q", "--cases", type=int, default=1,
+                        help="Cantidad de grillas a generar. Por defecto 1.")
+    parser.add_argument("-d", "--outdir", default='.',
                         help="Directorio de salida de los archivos")
     args = parser.parse_args()
 
-    config_file = Path(args.config_file)
+    config_file = Path(args.config)
     if config_file.is_file():
         create_grids(config_file, args)
     else:
         print("Error!")
-        print("Archivo {0} no encontrado".format(args.config_file))
+        print("Archivo {0} no encontrado".format(args.config))
 
 
 if __name__ == "__main__":
