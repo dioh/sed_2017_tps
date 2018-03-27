2c4d75f48d53eccbab050b451192a3b9a9957fbe
Modrow <hemodrow@tmoviles.com.ar>
2018-03-27 00:16:14 -0300

    Recursion del modelo para atomicos. Faltan procesar equation.

--- tpf/modelos-nested/modulosCDPP/cdpp_model.py
+++ tpf/modelos-nested/modulosCDPP/cdpp_model.py
@@ -1,32 +1,69 @@
 class CdppModel(object):
 
     def __init__(self, **kwargs):
-        allowed_keys = ['name', 'in_ports', 'out_ports',
+        allowed_keys = ['name', 'model','in_ports', 'out_ports',
                         'internal_connections',
                         'external_input_connections',
-                        'external_output_connections']
+                        'external_output_connections',
+                        'components',
+                        'type']
         self.name = ''
+        self.model = ''
         self.in_ports = set()
         self.out_ports = set()
         self.internal_connections = set()
         self.external_input_connections = set()
         self.external_output_connections = set()
+        self.components = set()
         # and update the given keys by their given values
         self.__dict__.update((key, value) for key, value
                              in kwargs.items() if key in allowed_keys)
 
+    def __repr__(self):
+        return str({'name': self.name,
+                    'model': self.model,
+                    'in_ports': self.in_ports.__repr__(),
+                    'out_ports': self.out_ports.__repr__(),
+                    'internal_connections':
+                    self.internal_connections.__repr__(),
+                    'external_input_connections':
+                    self.external_input_connections.__repr__(),
+                    'external_output_connections':
+                    self.external_output_connections.__repr__(),
+                    'components': self.components.__repr__()})
+
+    def __eq__(self, other):
+        return (self.name == other.name and
+                self.model == other.model and
+                self.in_ports == other.in_ports and
+                self.out_ports == other. out_ports and
+                self.internal_connections == other.internal_connections and
+                self.external_input_connections == other.external_input_connections and 
+                self.external_output_connections == other.external_output_connections and
+                self.components == other.components)
+
+    def __hash__(self):
+        return (hash(self.name) ^ hash(self.model))
+
     @classmethod
     def from_devsml_xml(cls, devsml_xml):
         root = devsml_xml.getroot()
-        model_name = root.attrib['name']
-        d = {'name': root.attrib['name'],
-             'in_ports': cls.extract_in_ports(root, model_name),
-             'out_ports': cls.extract_out_ports(root, model_name),
-             'internal_connections': cls.extract_internal_connections(root),
+        return cls.extract_model_from_node(root)
+
+    @classmethod
+    def extract_model_from_node(cls, node):
+        model_name = node.attrib['name']
+        d = {'name': model_name,
+             'model': node.attrib['model'],
+             'params': cls.extract_params(node.attrib),
+             'in_ports': cls.extract_in_ports(node, model_name),
+             'out_ports': cls.extract_out_ports(node, model_name),
+             'components': cls.extract_components(node),
+             'internal_connections': cls.extract_internal_connections(node),
              'external_input_connections':
-             cls.extract_external_input_connections(root, model_name),
+             cls.extract_external_input_connections(node, model_name),
              'external_output_connections':
-             cls.extract_external_output_connections(root, model_name)}
+             cls.extract_external_output_connections(node, model_name)}
 
         return cls(**d)
 
@@ -34,8 +71,7 @@ class CdppModel(object):
     def extract_in_ports(cls, devsml_xml_root, component_name):
         rv = set()
         for in_port in devsml_xml_root.findall('./inputs/input'):
-            if (in_port.attrib['component'] == component_name and
-               in_port.attrib['type'].lower() == 'in'):
+            if (in_port.attrib['type'].lower() == 'in'):
                 rv.add(in_port.attrib['name'])
 
         return rv
@@ -44,8 +80,7 @@ class CdppModel(object):
     def extract_out_ports(cls, devsml_xml_root, component_name):
         rv = set()
         for out_port in devsml_xml_root.findall('./outputs/output'):
-            if (out_port.attrib['component'] == component_name and
-               out_port.attrib['type'].lower() == 'out'):
+            if (out_port.attrib['type'].lower() == 'out'):
                 rv.add(out_port.attrib['name'])
 
         return rv
@@ -85,17 +120,34 @@ class CdppModel(object):
     @classmethod
     def extract_external_output_connections(cls, devsml_xml_root, model_name):
         rv = set()
-        internal_connections_path = './external_output_connections/connection'
-        internal_connections = devsml_xml_root.findall(
-            internal_connections_path)
-        for internal_connection in internal_connections:
-            attribs = internal_connection.attrib
+        connections_path = './external_output_connections/connection'
+        internal_out_connections = devsml_xml_root.findall(
+            connections_path)
+        for internal_out_connection in internal_out_connections:
+            attribs = internal_out_connection.attrib
             connection = CdppConnection(CdppPort(attribs['port_from'],
                                                  attribs['component_from']),
                                         CdppPort(attribs['port_to'],
                                                  model_name))
             rv.add(connection)
-        
+
+        return rv
+
+    @classmethod
+    def extract_components(cls, devsml_xml_root):
+        rv = set()
+        atomicsRef_path = './components/atomicRef'
+        atomicsRef = devsml_xml_root.findall(atomicsRef_path)
+        for atomicRef in atomicsRef:
+            rv.add(cls.extract_model_from_node(atomicRef))
+
+        return rv
+
+    @classmethod
+    def extract_params(cls, devsml_xml_attribs):
+        rv = dict(devsml_xml_attribs)
+        del rv['name']
+        del rv['model']
         return rv
 
 
